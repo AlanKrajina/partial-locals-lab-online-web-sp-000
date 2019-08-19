9fc2a202eb76fd85294789bd2c04be936d457c65
Alan Krajina <alankrajina88@gmail.com>
2019-08-19 16:17:52 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,7 +10,7 @@
 #  updated_at :datetime         not null
 #
 
-class Student < ActiveRecord::Base
+ Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
